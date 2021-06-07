7503ecc96c7911daad4cd90069847cb89625c4bc
PallavGarg2002 <83810910+PallavGarg2002@users.noreply.github.com>
2021-06-07 11:29:43 +0530

    Update script.js

--- mod4/script.js
+++ mod4/script.js
@@ -1,12 +1,12 @@
 var names=new Array();
 names[0]="Yaakov";
-names[1]="Junaid";
+names[1]="John";
 names[2]="jen";
-names[3]="jameson";
-names[4]="palak";
+names[3]="jason";
+names[4]="paul";
 names[5]="frank";
 names[6]="larry";
-names[7]="jerry";
+names[7]="Paula";
 names[8]="laura";
 names[9]="jim";
 
